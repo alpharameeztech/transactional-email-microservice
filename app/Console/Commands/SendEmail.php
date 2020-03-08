38121da9647f1e01b9eb60f7397ff23d47eb7e98
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 20:17:23 +0500

    Create a simple CLI command 'php artisan send:email' which will ask recipient email's, message and subject to send as an email. This CLI command directly sends out the email without saving the data in the database.

---
+++ app/Console/Commands/SendEmail.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+
+class SendEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Provide recipient email, subject and message to send an email';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $to = $this->ask('What is the recipient\'s email?');
+        $subject = $this->ask('What is the email subject?');
+        $message = $this->ask('What is the email message?');
+
+        $validator = Validator::make([
+            'to' => $to,
+            'subject' => $subject,
+            'message' => $message
+        ], [
+            'to' => ['required'],
+            'subject' => ['required','max:255'],
+            'message' => ['required', 'max:255']
+        ]);
+
+        $this->info('Sit tight, email is on its way...');
+
+        $data = array(
+            'to' => $to,
+            'subject' => $subject,
+            'message' => $message
+        );
+
+        /*
+         * send an email
+         * with queue job
+         */
+        \App\Jobs\SendEmail::dispatch($data)
+            ->onQueue('email');
+
+        $this->info('Email successfully sent to ' . $to . ' on ' . Carbon::now());
+    }
+}
