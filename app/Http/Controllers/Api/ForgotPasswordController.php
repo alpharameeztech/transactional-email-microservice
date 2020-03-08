6d4afb303c57ff63870f1bc3f5a5f0d24f9853fb
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 19:30:53 +0500

    Create an API controller that will handle the forgot password request and return the token on the response if found the email record on the database

---
+++ app/Http/Controllers/Api/ForgotPasswordController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Validation\ValidationException;
+use Validator;
+use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
+class ForgotPasswordController extends ApiController
+{
+
+    /*
+   |--------------------------------------------------------------------------
+   | Forgot Password Controller
+   |--------------------------------------------------------------------------
+   |
+   | This controller is responsible for generating tokens if the
+   | valid email(appears on the database) is provided on the post request.
+   | Here we are utilizing the SendsPasswordResetEmails trait
+   | which comes out of the box. Feel free to explore this trait.
+   |
+    */
+
+    use SendsPasswordResetEmails;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware('guest');
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param Request $request
+     * @return HttpResponse
+     * @throws ValidationException
+     */
+    public function getResetToken(Request $request)
+    {
+
+        /*
+         * make sure the request has an email field
+         */
+        $this->validate($request, ['email' => 'required|email']);
+
+        if ($request->wantsJson()) {
+
+            $user = User::where('email', $request->input('email'))->first();
+
+            /*
+             * generate a token for the password recovery
+             */
+            $token = $this->broker()->createToken($user);
+
+            /*
+             *  Save the response
+             *  to pass it after logging
+             */
+            $response = $this->respondCreated([
+                'result' => 'Token is created successfully.',
+                'token' => $token
+            ]);
+
+            /*
+             *  Log the success response
+             */
+            $this->log($response);
+
+            return $response;
+
+        }
+    }
+
+
+}
