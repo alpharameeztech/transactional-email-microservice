4a9696210e468e71a039493901b7868b724da615
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 16:50:42 +0500

    Register the email service providers aliases

--- config/app.php
+++ config/app.php
@@ -225,6 +225,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'MailJet' => App\Interfaces\EmailInterface\Implementations\MailJet::class,
+        'SendGrid' => App\Interfaces\EmailInterface\Implementations\SendGrid::class
 
     ],
 
