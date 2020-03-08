50a9796ec7893607df25546a390cf7b409782ae4
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 17:28:13 +0500

    Make sure the app send the email with the default email service provider

--- tests/Unit/SendEmailTest.php
+++ tests/Unit/SendEmailTest.php
@@ -67,4 +67,88 @@ class SendEmailTest extends TestCase
 
 
     }
+
+    /**
+     * Send an email with the default
+     * email service provider
+     * with valid recipient's email,
+     * subject and message
+     *
+     * @test
+     */
+    public function send_an_email_with_default_email_service_provider()
+    {
+        /*
+        * Prepare email's data to send
+        */
+        $data = [
+            'to' => $this->faker->email,
+            'subject' => $this->faker->sentence,
+            'message' => $this->faker->paragraph
+        ];
+
+        /*
+         * Send an email and save the response
+         */
+        $response = $this->json('Post', route('send.email'), $data, [
+            'Content-Type' => 'application/json'
+        ]);
+        /*
+        * The email service used to send this email
+        */
+        $emailServiceUsedToSendEmail = $response['data']['message']['service'];
+
+        /*
+         *  The default email service
+         */
+        $defaultEmailService = config('mail.service');
+
+        $this->assertEquals($defaultEmailService, $emailServiceUsedToSendEmail);
+
+    }
+
+    /**
+     * Send an email with a fallback service
+     * if the something went wrong when
+     * try to send an email with the default one
+     *
+     * @test
+     */
+    public function send_an_email_with_a_fallback_email_service_provider()
+    {
+        /*
+         *  The default email service
+         */
+        $defaultEmailService = config('mail.service');
+
+        /*
+        * Prepare email's data to send
+        */
+        $data = [
+            'to' => $this->faker->email,
+            'subject' => $this->faker->sentence,
+            'message' => $this->faker->paragraph
+        ];
+
+        /*
+         * Fail a job
+         */
+        $exception = new Exception();
+        $sendEmailJob = new SendEmail($data);
+        $response = $sendEmailJob->failed($exception);
+
+        /*
+         * Send an email and save the response
+         */
+        $response = $this->json('Post', route('send.email'), $data, [
+            'Content-Type' => 'application/json'
+        ]);
+
+        /*
+        * The email service used to send this email
+        */
+        $emailServiceUsedToSendEmail = $response['data']['message']['service'];
+
+        $this->assertNotEquals($defaultEmailService, $emailServiceUsedToSendEmail);
+    }
 }
