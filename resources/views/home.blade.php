88d82e250ac1e675d4eb8758807ad95d8192992e
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 20:52:07 +0500

    Create a parent blade view file that other blade files can extend it. Rename the welcome blade file.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('title', 'Takeaway.com')
+
+@section('page-title')
+    @yield('title')
+@endsection
+
+@section('content')
+
+    <div class="">
+
+        <div class="my-2">
+            asdasd
+        </div>
+
+    </div>
+
+@endsection
