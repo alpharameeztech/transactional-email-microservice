43c54a77b6a3544bdd52a5149772093cd7e6cef8
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 21:35:26 +0500

    Create a navigation vue component as a parent vue page whick will have the router view to routes to different pages

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,119 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer
+            v-model="drawer"
+            :clipped="$vuetify.breakpoint.lgAndUp"
+            app
+        >
+            <v-list dense>
+                <template v-for="item in items">
+                    <v-row
+                        v-if="item.heading"
+                        :key="item.heading"
+                        align="center"
+                    >
+                        <v-col cols="6">
+                            <v-subheader v-if="item.heading">
+                                {{ item.heading }}
+                            </v-subheader>
+                        </v-col>
+                        <v-col
+                            cols="6"
+                            class="text-center"
+                        >
+                            <a
+                                href="#!"
+                                class="body-2 black--text"
+                            >EDIT</a>
+                        </v-col>
+                    </v-row>
+                    <v-list-group
+                        v-else-if="item.children"
+                        :key="item.text"
+                        v-model="item.model"
+                        :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                        append-icon=""
+                    >
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{ item.text }}
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+                        <v-list-item
+                            v-for="(child, i) in item.children"
+                            :key="i"
+                            link
+                        >
+                            <v-list-item-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{ child.text }}
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-group>
+                    <v-list-item
+                        v-else
+                        :key="item.text"
+                        link
+                        :to="item.link"
+                    >
+                        <v-list-item-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                {{ item.text }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+            :clipped-left="$vuetify.breakpoint.lgAndUp"
+            app
+            color="blue darken-3"
+            dark
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+            <v-toolbar-title
+                style="width: 300px"
+                class="ml-0 pl-4"
+            >
+                <a href="/">
+                    <span class="hidden-sm-and-down">Takeaway.com</span>
+                </a>
+            </v-toolbar-title>
+            <v-spacer />
+        </v-app-bar>
+        <v-content>
+
+            <flash-component></flash-component>
+
+            <router-view></router-view>
+
+            <loader-component></loader-component>
+
+        </v-content>
+
+    </v-app>
+</template>
+
+<script>
+    export default {
+        props: {
+            source: String,
+        },
+        data: () => ({
+            dialog: false,
+            drawer: null,
+            items: [],
+        }),
+    }
+</script>
