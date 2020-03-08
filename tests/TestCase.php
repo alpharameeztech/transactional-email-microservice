e88dbb7594a22dbc0f0473768679333767bbae8b
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 17:00:52 +0500

    Make available the faker class object on every test class that extends the base class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use Faker\Factory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    public $faker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Factory::create();
+    }
 }
