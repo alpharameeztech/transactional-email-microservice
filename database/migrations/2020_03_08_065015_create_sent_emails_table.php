018fcdf54d251094abfd22579e89863132421026
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 11:51:56 +0500

    Create a table that will store the record of the emails that are sent from the API endpoint.

---
+++ database/migrations/2020_03_08_065015_create_sent_emails_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSentEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sent_emails', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('to');
+            $table->string('subject');
+            $table->text('message');
+            $table->text('service');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sent_emails');
+    }
+}
