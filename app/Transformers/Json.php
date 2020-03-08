0d42e8f283a17dc74e9d9733195cc20ecd68b9c3
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 15:22:21 +0500

    Create a simple class that will return the json response

---
+++ app/Transformers/Json.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Transformers;
+
+class Json
+{
+    public static function response($data = null, $message = null)
+    {
+        return [
+            'data'    => $data,
+            'message' => $message,
+        ];
+    }
+}
