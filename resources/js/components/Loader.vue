f4d68a62c672f30e55c1324cf0bbc497d60816cc
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-08 21:24:04 +0500

    Create a loader vue component using vuetify

---
+++ resources/js/components/Loader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="text-center">
+        <v-overlay :value="overlay" >
+            <v-progress-circular indeterminate size="64"></v-progress-circular>
+        </v-overlay>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            overlay: false,
+        }),
+        methods: {
+
+        },
+        mounted() {
+            this.$root.$on('loading', data => {
+                this.overlay = data
+            });
+        }
+    }
+</script>
