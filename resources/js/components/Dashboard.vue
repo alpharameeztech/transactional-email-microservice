4eb29a08a48778127e222d4f99d09d2a5ad05e78
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-03-09 17:49:06 +0500

    Add the description the dashboard page about how to use this app to send an email

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -158,6 +158,192 @@
             </li>
         </ul>
 
+        <p class="title">
+            Send an email through an API endpoint
+        </p>
+        <p>
+            API link: <kbd>[site.url]/api/v1/send/email</kbd>
+        </p>
+        <p>
+            Headers: <code>Content-Type => application/json</code>
+        </p>
+        <p>
+            Pass the data in json format including the following fields:
+        </p>
+        <ul>
+            <li>Recipient's email as: 'to'</li>
+            <li>Email's subject as: 'subject'</li>
+            <li>Email's message body as: 'subject'</li>
+        </ul>
+        <p>
+            After sending the post request with the fields, the app will first try to send an email
+            with the default email service provider(as configured on 'config/mail.php').
+            If for any reason, the default service is down, the app will pick any fallback service from the array
+            defined in the '/config.mail.php' and send an email through that.
+            Once the email is send, then the following actions are performed:</p>
+
+        <ul>
+            <li>API response will receive.
+            </li>
+            <li>
+                The response will have the status code and the email service provider name
+                through which the email is sent
+            </li>
+            <li>The record is saved on the database table 'sent_emails'</li>
+            <li>A log entry is also created under 'laravel.log' file</li>
+        </ul>
+
+        <p>Take a look at the below screenshot which shows how to call this API endpoint using Postman.</p>
+
+        <img src="https://somefreeresources.s3.ca-central-1.amazonaws.com/send_email_with_api.png" />
+
+        <p class="font-weight-bold">
+            If invalid data is provided on send email API, an appropriate error response is returned, something similar to the screenshot below
+        </p>
+
+        <img src="https://somefreeresources.s3.ca-central-1.amazonaws.com/error_response_on_send_email.png" />
+
+        <v-system-bar
+            dark
+            color="primary"
+            height="5"
+        ></v-system-bar>
+
+        <p class="title">
+            Send an email through CLI
+        </p>
+
+        <p>
+            A command is created to send an email from CLI
+        </p>
+        <p>
+            Run the following command to learn about this send email command:  <kbd> php artisan send:email --help</kbd>
+        </p>
+
+        <p>
+            Run this command to send an email from the CLI: <kbd>php artisan send:email</kbd>
+        </p>
+
+        <p>Once you run the command, the CLI will prompt for the following fields:</p>
+        <ul>
+            <li>
+                Recipient's email
+            </li>
+            <li>
+                Email's subject
+            </li>
+            <li>
+                Email's body
+            </li>
+        </ul>
+
+        <p>Something like that will appear</p>
+
+        <img src="https://somefreeresources.s3.ca-central-1.amazonaws.com/send_email_from_cli.png" />
+
+        <p>
+            If you do not provide any of required fields, an error will appear on the CLI asking you for the missing fields.
+        </p>
+        <p>
+            With CLI, only the log entry is created(as shown in the below screenshot) when the email is sent(wont be storing the under the database).
+        </p>
+        <img src="https://somefreeresources.s3.ca-central-1.amazonaws.com/cli_log_entery.png" />
+
+        <v-system-bar
+            dark
+            color="primary"
+            height="5"
+        ></v-system-bar>
+
+        <p class="title">
+            Send an email through Frontend
+        </p>
+        <p>
+            A form is set up to compose an email.
+        </p>
+
+        <p>When the email is successfully sent, following actions are performed:</p>
+
+        <ul>
+            <li>
+                An email will be sent with either the default email service provider(if no exception is thrown) or with the fallback service
+            </li>
+            <li>
+                A log entry is created under 'laravel.log' file
+            </li>
+            <li>The entry will be saved under the 'sent_emails' table</li>
+        </ul>
+
+        <img src="https://somefreeresources.s3.ca-central-1.amazonaws.com/send_email_from_frontend.png" />
+
+        <v-system-bar
+            dark
+            color="primary"
+            height="5"
+        ></v-system-bar>
+
+        <p class="title">
+            Consumer self-service endpoint for user registration
+        </p>
+        <p>
+            API link: <kbd>[site.url]/api/v1/register</kbd>
+        </p>
+        <p>
+            A consumer self-service API endpoint exist to register a user.
+        </p>
+        <p>
+            This endpoint requires the following required fields:
+        </p>
+
+        <ul>
+            <li>
+                User name field as: 'name'
+            </li>
+            <li>
+                User email fields as: 'email'
+            </li>
+            <li>
+                User's password field as: 'password'
+            </li>
+            <li>
+                User's confirm password field as: 'password_confirmation'
+            </li>
+        </ul>
+
+        <p>
+            When the user is successfully registered, the following actions are performed:
+        </p>
+        <ul>
+            <li>
+                A json response received which will show the success message along with the service name used to send the welcome email
+            </li>
+
+            <img src="https://somefreeresources.s3.ca-central-1.amazonaws.com/user_registration_api.png" />
+
+            <li>
+                A log entry created
+            </li>
+
+            <img src="https://somefreeresources.s3.ca-central-1.amazonaws.com/user_registration_log_entry.png" />
+
+            <li>
+                User is successfully saved on the database
+            </li>
+            <li>
+                A welcome email is sent to the user's email just registered
+            </li>
+        </ul>
+
+        <p class="font-weight-bold">If user registration failed for any reason, an appropriate error response is returned</p>
+
+        <img src="https://somefreeresources.s3.ca-central-1.amazonaws.com/user_registration_error.png" />
+
+        <v-system-bar
+            dark
+            color="primary"
+            height="5"
+        ></v-system-bar>
+
     </v-container>
 </template>
 
